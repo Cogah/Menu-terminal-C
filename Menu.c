46a7367e3f6f3aca85b7ca4685680a85903344ea
Gabriel Coelho Góes <161400667+Cogah@users.noreply.github.com>
2025-02-19 22:32:50 -0300

    Add files via upload

---
+++ Menu.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#define ANOATUAL 2024 // fixa o ano com que será feito o cálculo de idade(nesse caso, 2024)
+
+int main(int argc, char *argv[]) {
+    char op;
+    char menu[60] = "Menu\na)calc idade\nb)vogal\nc)trocar cor\nx)sair do programa";
+
+    while(1){
+        system("cls");
+        puts(menu);
+        op = getche();
+
+        //OPÇÃO A
+        if(op == 'a'){
+            system("cls");
+            int anonasc, idade;
+
+            printf("Digite seu ano de nascimento: ");
+            scanf("%d",&anonasc);
+
+            idade = ANOATUAL - anonasc;
+
+            printf("\nVoce tem %d anos\n", idade);
+
+            if(idade >= 18){
+                printf("Em %d voce e maior de idade", ANOATUAL);
+            }else{
+                printf("Em %d voce e menor de idade", ANOATUAL);
+            }
+
+            printf("\n\nPressione qualquer tecla para voltar ao menu...");
+        }
+        //OPÇÃO B
+        if(op == 'b'){
+            char vogais[10] = "aeiouAEIOU";
+            char vogal;
+            int i;
+
+            i = rand()%10;
+            vogal = vogais[i];
+
+            printf("\nSua vogal: %c", vogal);
+        }
+        //OPÇÃO X
+        if(op == 'x'){
+            return 0;
+        }
+
+        system("pause>null");
+    }
+
+}
